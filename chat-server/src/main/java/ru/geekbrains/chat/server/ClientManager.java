a8dc390c39566acbde10483edb46687c03d49b0b
Alexander <frosti-sizov@rambler.ru>
2023-12-28 16:14:45 +0300

    test

---
+++ chat-server/src/main/java/ru/geekbrains/chat/server/ClientManager.java
@@ -0,0 +1,93 @@
+package ru.geekbrains.chat.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientManager implements Runnable {
+
+    private Socket socket;
+
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    private String name;
+
+
+
+    public ClientManager(Socket socket){
+        try {
+            this.socket = socket;
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            name = bufferedReader.readLine();
+            ClientManagerSingleton.getInstance().add(this);
+            System.out.println(name + " подключился к чату.");
+            broadcastMessage("Server: " + name + " подключился к чату.");
+        }
+        catch (IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+
+    }
+
+
+    private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+
+        removeClient();
+        try {
+
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void removeClient(){
+        ClientManagerSingleton.getInstance().remove(this);
+        System.out.println(name + " покинул чат.");
+    }
+
+    @Override
+    public void run() {
+        String massageFromClient;
+
+
+        while (socket.isConnected()){
+            try {
+
+                massageFromClient = bufferedReader.readLine();
+                broadcastMessage(massageFromClient);
+            } catch (IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+
+    private void broadcastMessage(String massage) {
+        for (ClientManager client: ClientManagerSingleton.getInstance()) {
+            try {
+
+                if (!client.name.equals(name)) {
+                    client.bufferedWriter.write(massage);
+                    client.bufferedWriter.newLine();
+                    client.bufferedWriter.flush();
+                }
+            } catch (IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+}
